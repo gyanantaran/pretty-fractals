a701c527f9e5ec41e52ad48761b2d459adbbd43c
Vishal Paudel <95016059+vishalpaudel@users.noreply.github.com>
2023-04-16 21:40:09 +0530

    imporvements

--- julia_mandelbrot/vishal_paudel_mandelbrot.jl
+++ julia_mandelbrot/vishal_paudel_mandelbrot.jl
@@ -1,71 +1,36 @@
-#=
-# AUTHOR: Vishal Paudel
-# DATE: Sat Apr 16 11:13:11 IST 2023
-#
-# NOTE: None
-# TODO: None
-=#
-
 using Plots
 
 function cond(z::Complex)
-    return (abs2(z)) > 4
+    return abs2(z) > 4
 end
-
 
 function f(z::Complex, c::Complex)
-    return c * c + z * z * z
+    return c^2 + z^3
 end
 
-
-function iterCount(z0::Complex, C::Complex, maxIter::Int)
+function iter_count(z0::Complex, c::Complex, max_iter::Int)
     z = z0
-    c = C
-    for iter = 1:1:maxIter
+    for iter in 1:max_iter
         z = f(z, c)
-        if cond(z)
-            return iter
-        end
+        cond(z) && return iter
     end
-    return maxIter + 1  # to show more than maxIter
+    return max_iter + 1
 end
 
-
-function returnArray(size::Int)
-    Z0 = Complex(-0.1, -0.2) # 0 + 0im
-    C = Complex(0.0, 0.0)
-
-    # Sqare only
-    mandelArr = zeros(Int, (size, size))
-    juliaArr = zeros(Int, (size, size))
-
-    # Odd values can leave one line of pixels uncoloured
-    scale = 0.25
-    origin = 0 + 0im
-
-    function point(i::Int, j::Int)
-        x = (j / size - 1 / 2)
-        y = (1 / 2 - i / size)
-        z = ((x + y * im) + origin) / scale
-        return z
-    end
-
-    maxIter = 30
-    for i = 1:1:size
-        for j = 1:1:size
-            p = point(i, j)
-            z0 = p
-            c = p
-
-            mandelArr[i, j] = iterCount(Z0, c, maxIter)
-            juliaArr[i, j] = iterCount(z0, C, maxIter)
-        end
-    end
-
-    return mandelArr, juliaArr
+function return_array(size::Int, z0::Complex=-0.1-0.2im, c::Complex=0.0+0.0im, max_iter::Int=30, scale::Float64=0.25, origin::Complex=0+0im)
+    mandel_arr = [iter_count(point(i, j, size, scale, origin), point(i, j, size, scale, origin), max_iter) for i in 1:size, j in 1:size]
+    julia_arr = [iter_count(point(i, j, size, scale, origin), c, max_iter) for i in 1:size, j in 1:size]
+    return mandel_arr, julia_arr
 end
 
-mandel, julia = returnArray(2000)
+function point(i::Int, j::Int, size::Int, scale::Float64, origin::Complex)
+    x = (j / size - 1 / 2)
+    y = (1 / 2 - i / size)
+    z = ((x + y * im) + origin) / scale
+    return z
+end
 
-Plots.heatmap(mandel, color=:inferno)
-# Plots.heatmap(julia, yflip=true, color=:inferno)
+mandel, julia = return_array(2000)
+
+heatmap(mandel, color=:inferno)
+# heatmap(julia, yflip=true, color=:inferno)
